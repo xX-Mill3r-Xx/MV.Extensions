cec48b10222167a2da5349442d1bd253f2cf7c57
Miller M <86807590+xX-Mill3r-Xx@users.noreply.github.com>
2026-02-15 23:11:59 -0300

    🚀  feat: initial commit

--- TextBoxExtension.cs
+++ TextBoxExtension.cs
@@ -21,5 +21,22 @@
         {
             return numeroDecimal;
         }
+
+        public static string RemoverAcentos(string texto)
+        {
+            if (string.IsNullOrEmpty(texto))
+                return texto;
+
+            var normalized = texto.Normalize(System.Text.NormalizationForm.FormD);
+            var sb = new System.Text.StringBuilder();
+            foreach (var item in normalized)
+            {
+                if (System.Globalization.CharUnicodeInfo.GetUnicodeCategory(item) != 
+                    System.Globalization.UnicodeCategory.NonSpacingMark)
+                    sb.Append(item);
+            }
+
+            return sb.ToString().Normalize(System.Text.NormalizationForm.FormC);
+        }
     }
 }
